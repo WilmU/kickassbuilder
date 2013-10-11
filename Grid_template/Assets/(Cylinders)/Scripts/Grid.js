6310c8bb8db58abfbaedbfe52c419ee55a01ec2c
WilmU <student@NCP034-13674.local>
2013-10-11 11:47:08 -0400

    Testing
    This is to test the repo

---
+++ Grid_template/Assets/(Cylinders)/Scripts/Grid.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+	public var ball : GameObject;
+	private var playerBall : GameObject;
+	public var spacing : float;
+	
+	var startCube : GameObject;
+	var boundriesZ : float;
+	var boundriesX : float;
+	
+function Start () {
+
+	var aP : GameObject;
+	var numRows: int = 9;
+	var numCols: int = 9;
+	
+	for(var row:int = 0; row <= numRows; row++)
+	{
+		for(var col:int = 0; col <= numCols; col++)
+		{
+			aP = GameObject. CreatePrimitive(PrimitiveType.Cube);
+			aP.transform.position.z = col * spacing;
+			aP.transform.position.x = row * spacing;
+			aP.name = "box_" + col + "_" + row;
+		}
+	}
+	playerBall = Instantiate(ball,Vector3(0,1,0),Quaternion.identity);
+	startCube = GameObject.Find("box_0_0");
+	playerBall.transform.position.x = startCube.transform.position.x;
+	playerBall.transform.position.z = startCube.transform.position.z;
+	var info : ballMovement = playerBall.GetComponent(ballMovement);
+	info.moveSpace = spacing;
+	
+	boundriesZ = numCols * spacing;
+	boundriesX = numRows * spacing;
+	info.newBoundriesZ = boundriesZ;
+	info.newBoundriesX = boundriesX;
+	
+}
---
+++ Grid_template/Assets/(Cylinders)/Scripts/ballMovement.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+public var moveSpace : float;
+private var moving : boolean;
+
+var newBoundriesZ : float;
+var newBoundriesX : float;
+
+function Start () {
+
+}
+
+function Update () {
+	if (moving == false)
+	{
+		if (Input.GetKeyDown("w") && transform.position.z < newBoundriesZ)
+		{
+			transform.position.z += moveSpace;
+			}
+		if (Input.GetKeyDown("a") && transform.position.x > 0)
+		{
+			transform.position.x -= moveSpace;
+			}
+		if (Input.GetKeyDown("s") && transform.position.z > 0)
+		{
+			transform.position.z -= moveSpace;
+			}
+		if (Input.GetKeyDown("d") && transform.position.x < newBoundriesX)
+		{
+			transform.position.x += moveSpace;
+			}
+		
+	}
+
+}
